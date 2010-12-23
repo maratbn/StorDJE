f5b7250616aa139e0bf6cd2ea18aeddd5c68bccc
Marat Nepomnyashy <maratbn@gmail.com>
2010-12-23 00:41:31 -0800

    Added 'stordje-release-0.3-min.js'.

---
+++ stordje-release-0.3-min.js
@@ -0,0 +1,8 @@
+/*
+StorDJE - Storage layer add-on module for the Dojo Toolkit event system version 0.3
+Copyright (C) 2010 Marat Nepomnyashy All Rights Reserved.
+Licensed under BSD, see: https://github.com/maratbn/StorDJE/raw/master/LICENSE
+This file is optimized for deployment, for source see: https://github.com/maratbn/StorDJE/
+*/
+
+dojo.provide('stordje');(function(){stordje.publish=function(topic,args){if(!stordje._topics)stordje._topics={};stordje._topics[topic]=args;dojo.publish.apply(this,arguments);};stordje.recall=function(topic){return stordje._topics&&stordje._topics[topic]||null;};stordje.recallFirst=function(topic){var args=stordje.recall(topic);return args&&args.length>0&&args[0]||null;};})()
